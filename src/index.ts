8ea36cf44adc8f5253ad9dd71850690a9162eb9e
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-10-10 10:07:25 +0900

    Transform transpiled code by TypeScript

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,20 @@ Object.defineProperty(COMPONENT, "name", {
 });
 `);
 
+const buildDefinitionForTypeScript = template(`
+Object.defineProperty(exports.COMPONENT, "name", {
+  value: COMPONENT_NAME
+});
+`);
+
 export default ({ types: t }: { types: typeof types }): PluginObj => ({
   name: 'babel-plubin-react-component-name',
   visitor: {
     Program(path) {
-      if (!path.scope.hasBinding('React')) {
+      if (
+        !path.scope.hasBinding('React') &&
+        !path.scope.hasBinding('react_1')
+      ) {
         return;
       }
 
@@ -25,6 +34,16 @@ export default ({ types: t }: { types: typeof types }): PluginObj => ({
           const componentName = getComponentName(path);
           if (isValidComponentName(componentName)) {
             const definition = buildDefinition({
+              COMPONENT: t.identifier(componentName),
+              COMPONENT_NAME: t.stringLiteral(componentName),
+            });
+            definitions.push(definition);
+          }
+        },
+        AssignmentExpression(path) {
+          const componentName = getComponentNameForTypeScript(path);
+          if (isValidComponentName(componentName)) {
+            const definition = buildDefinitionForTypeScript({
               COMPONENT: t.identifier(componentName),
               COMPONENT_NAME: t.stringLiteral(componentName),
             });
@@ -55,4 +74,20 @@ function isValidComponentName(
   componentName: string | null | undefined,
 ): componentName is string {
   return !!componentName && UPPER_PATTERN.test(componentName);
+}
+
+function getComponentNameForTypeScript(path: NodePath<types.AssignmentExpression>) {
+  const expression = path.node.left;
+  if (
+    !types.isMemberExpression(expression) ||
+    !types.isIdentifier(expression.object) ||
+    !types.isIdentifier(expression.property) ||
+    expression.object.name !== 'exports'
+  ) {
+    return;
+  }
+  if (!types.isFunctionExpression(path.node.right)) {
+    return;
+  }
+  return expression.property.name;
 }
